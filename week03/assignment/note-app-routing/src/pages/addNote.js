4460565bd0a01828c223ba34e8fbd26cd763be91
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-06 16:00:19 -0700

    Completed addNote and notesList page

--- week03/assignment/note-app-routing/src/pages/addNote.js
+++ week03/assignment/note-app-routing/src/pages/addNote.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HomePage from './home'
-import NotesList from './notesList'
-export default function AddNote() {
+import React, { useRef } from 'react'
+export default function AddNote({setNotes}) {
+
+    const noteMessageRef = useRef();
+    
+    // const [notes, setNotes] = useState([]);
+
+    function handleAddNote(e) {
+        const message = noteMessageRef.current.value
+        if (message === '') return 
+        setNotes(notes => {
+            return [...notes, { message: message }]
+        })
+        noteMessageRef.current.value = null
+    }
     return (
         <div>
             <h1>This is the Add Note Page</h1>
+            <input ref={noteMessageRef} type="text" />
+            <button onClick={handleAddNote}>Add Note</button>
         </div>
     )
 }
--- week03/assignment/note-app-routing/src/pages/notesList.js
+++ week03/assignment/note-app-routing/src/pages/notesList.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Note from './note';
 
-export default function NotesList() {
+export default function NotesList({notes}) {
+
+
     return (
         <div>
             <h1>This is the Note List Page</h1>
-            
+            <ul>
+                {
+                    notes.map(note => {
+                        return (
+                        <li>
+                            <Note message={note.message}/>
+                        </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
